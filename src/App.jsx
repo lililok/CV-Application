167ec4aba2b2eb5d39845e29c80e5dc037856461
Lililok <eshanu.sofia@yandex.ru>
2024-06-24 19:02:56 +0300

    structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,21 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import React from 'react';
 import './App.css'
+import GeneralForm from "./components/General.jsx";
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="forms">
+        <GeneralForm />
+        <div className="educationForm">b</div>
+        <div className="practiceForm">c</div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+      <div className="render">
+        <div className="generalRender">a</div>
+        <div className="educationRender">b</div>
+        <div className="practiceRender">c</div>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
---
+++ src/components/General.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function GeneralForm() {
+
+    return (
+        <>
+        <div className="generalForm">
+            hell yes
+        </div>
+        </>
+    )
+}
+
+export default GeneralForm;
